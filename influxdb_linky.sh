11d5697e60837cf4065d62e7d80b45123de5dc62
Ludovic Rousseau <ludovic.rousseau@free.fr>
2018-10-01 22:16:08 +0200

    Scripts to update an InfluxDB database

---
+++ influxdb_linky.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+set -au
+set -e
+
+SCRIPT="$0"
+BASE_DIR=$(dirname "${SCRIPT}")
+export BASE_DIR
+CFG_FILE="domoticz_linky.cfg"
+
+# check configuration file
+if [ -f "${BASE_DIR}"/"${CFG_FILE}" ]
+then
+  . "${BASE_DIR}"/"${CFG_FILE}"
+  export LINKY_USERNAME
+  export LINKY_PASSWORD
+  "${BASE_DIR}"/linky_influxdb.py > data.txt
+
+  curl -XPOST "$INFLUXDB_HOST/write?db=$INFLUXDB_DATABASE" \
+	  --user $INFLUXDB_USER:$INFLUXDB_PASSWORD \
+	  --data-binary @data.txt
+else
+    echo "Config file is missing ["${BASE_DIR}"/"${CFG_FILE}"]"
+    exit 1
+fi
---
+++ linky_influxdb.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""Generates energy consumption InfluxDB from Enedis (ERDF) consumption data
+collected via their website (API).
+"""
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import os
+import datetime
+import logging
+import linky_json
+
+USERNAME = os.environ['LINKY_USERNAME']
+PASSWORD = os.environ['LINKY_PASSWORD']
+
+def json_to_inflxudb(res, name):
+    for e in res:
+        print("Linky %s=%f %s" % (name, e['conso'], e['time']))
+
+
+# Main script
+def main():
+    logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
+
+    logging.info("logging in as %s...", USERNAME)
+    token = linky_json.linky.login(USERNAME, PASSWORD)
+    logging.info("logged in successfully!")
+
+    logging.info("retrieving data...")
+    today = datetime.date.today()
+
+    # Par heure
+    # Yesterday and the day before
+    res_hour = linky_json.linky.get_data_per_hour(token,
+            linky_json.dtostr(today - linky_json.relativedelta(days=2)),
+            linky_json.dtostr(today))
+
+    logging.info("got data!")
+
+    res_heure_json = linky_json.export_hours_values_json_format(res_hour,
+        "%s000000000")
+
+    json_to_inflxudb(res_heure_json, "heure")
+
+    # par jour
+    # données disponible depuis le 8/2/2018
+    res_day = linky_json.linky.get_data_per_day(token,
+        linky_json.dtostr(today - linky_json.relativedelta(days=1, months=1)),
+        linky_json.dtostr(today - linky_json.relativedelta(days=1)))
+
+    res_jour_json = linky_json.export_days_values_json_format(res_day,
+        "%s000000000")
+
+    json_to_inflxudb(res_jour_json, "jour")
+
+
+if __name__ == "__main__":
+    main()
